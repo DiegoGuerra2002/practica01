2c5df7f10dd0192229aa04639325b97ae0571a60
Diego <diego.guevara@catolica.edu.sv>
2023-03-24 16:39:56 -0600

    Cambios V2 del codigo

---
+++ practica.01/Controllers/carrerasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace practica._01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class carrerasController : ControllerBase
+    {
+    }
+}
---
+++ practica.01/Models/estados_equipo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace practica._01.Models
+{
+    public class estados_equipo
+    {
+        [Key]
+        public int? id_estados_equipo { get; set; }
+        public string? descripcion { get; set; }
+        public int? estado { get; set; }
+    }
+}
